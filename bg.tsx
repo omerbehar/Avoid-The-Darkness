a672c539aacc76bbecd70a84dde17821f52e3d55
omerbehar <omer.behar@gmail.com>
2021-12-15 10:34:45 +0200

    done for now

---
+++ bg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="bg" tilewidth="12" tileheight="12" tilecount="2209" columns="47">
+ <image source="bg.png" width="564" height="564"/>
+</tileset>
